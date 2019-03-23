316e1957f464cd8bd776045053a535c82a03d6e5
River Hermsen <riverhermsen@hotmail.nl>
2019-03-23 21:38:54 +0100

    Initial express server with route files

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb://admin:admin123@ds157204.mlab.com:57204/mern-course'
+}
---
+++ routes/api/posts.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+// @router  GET api/posts/test
+// @desc    Test Post route
+// @access  Public
+router.get('/test', (req, res) => res.json({
+    msg: "Posts Works"
+}));
+
+module.exports = router;
