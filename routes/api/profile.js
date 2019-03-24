86e4ec0575bcc44860b581c1027ef9b30e23acf6
River Hermsen <riverhermsen@hotmail.nl>
2019-03-24 22:29:54 +0100

    Added route to get current users profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+// Load profile modal
+const Profile = require('../../models/Profile');
+// Load User modal
+const User = require('../../models/User');
 
 // @router  GET api/profile/test
 // @desc    Test Profile route
@@ -7,5 +14,23 @@ const router = express.Router();
 router.get('/test', (req, res) => res.json({
     msg: "Profile Works"
 }));
+
+// @router  GET api/profile
+// @desc    Get current users profile
+// @access  Private
+router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+    const errors = {};
+
+    Profile.findOne({ user: req.user.id })
+        .then(profile => {
+            if (!profile) {
+                errors.noprofile = 'There is no profile for this user';
+                return res.status(404).json(errors)
+            }
+            res.json(profile)
+        })
+        .catch(err => res.status(404).json(err))
+});
+
 
 module.exports = router;
